0019bd4905e408b25899c12fbff89c0e1ad08fe1
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-27 17:01:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " "
-    return ""
+    return "false"
+  else board[index] == ""
+    return "false"
+  else board[index] == nil  
+    return "false"
+  elsif  
